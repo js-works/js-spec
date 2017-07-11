9ff6b66cb15bc4b1da791b3446ebae2b03b01e5d
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2017-07-11 04:51:03 +0200

    Some updates

--- src/main/js-spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Spec_1 = require("./api/Spec");
-exports.Spec = Spec_1.default;
-var SpecError_1 = require("./api/SpecError");
-exports.SpecError = SpecError_1.default;
-//# sourceMappingURL=js-spec.js.map
