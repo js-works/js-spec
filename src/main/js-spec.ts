89dc67b255ddabddbf47c8ef93fc4142afa7174d
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2017-07-14 03:23:36 +0200

    Some updates

--- src/main/js-spec.ts
+++ src/main/js-spec.ts
@@ -1,9 +1,13 @@
+import createSpecValidator from './api/createSpecValidator';
 import isSpecValidator from './api/isSpecValidator';
 import Spec from './api/Spec';
 import SpecError from './api/SpecError';
+import SpecValidator from './api/SpecValidator';
 
 export {
+    createSpecValidator,
     isSpecValidator,
     Spec,
-    SpecError
+    SpecError,
+    SpecValidator
 }
