d70b5a4e9cb019d81b5dee280e265e4fe59488f2
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-05-31 10:28:23 +0200

    Some udpates

--- src/main/api/Validator.ts
+++ src/main/api/Validator.ts
@@ -4,6 +4,6 @@ import SpecError from './SpecError';
 type Validator =
   //{ validate(it: any, path: string | null): boolean | string | Error | null } 
   SpecValidator
-  | { (it: any, path: string | null): boolean | string | Error | null };
+  | { (it: any, path?: string | null): boolean | string | Error | null };
 
 export default Validator;
