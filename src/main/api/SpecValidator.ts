703f8695c1b8fb84acc7f9d0555af73b6e41c067
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2018-09-23 23:53:44 +0200

    Some updates

--- src/main/api/SpecValidator.ts
+++ src/main/api/SpecValidator.ts
@@ -1,7 +1,7 @@
 import SpecError from './SpecError'
 
 export default interface SpecValidator {
-  (it: any, path: string | null): SpecError | null
+  (it: any, path?: string | null): SpecError | null
   validate(it: any, path?: string | null): SpecError | null
   usingHint(hint: string): SpecValidator
 }
