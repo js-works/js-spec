03b3eb4380828ad87ef9e74236b1616427812d41
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-01-02 13:10:59 +0100

    Deleted unimportant files

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-// TODO - do not know why "export * from ..." is not working
-import Spec from './dist/types/api/Spec';
-import SpecError from './dist/types/api/SpecError';
-import SpecValidator from './dist/types/api/SpecValidator';
-import Validator from './dist/types/api/Validator';
-export { Spec, SpecError, SpecValidator, Validator };
