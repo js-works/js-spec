86b0c77508cac19d6c8192810bda14d62f9949fd
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-09-23 11:41:17 +0200

    Some fixes regarding typing

--- src/main/api/SpecValidator.ts
+++ src/main/api/SpecValidator.ts
@@ -1,7 +1,7 @@
 import SpecError from './SpecError'
 
-export default interface SpecValidator {
-  (it: any, path?: string | null): SpecError | null
-  validate(it: any, path?: string | null): SpecError | null
+export default interface SpecValidator<T = any> {
+  (it: T, path?: string | null): SpecError | null
+  validate(it: T, path?: string | null): SpecError | null
   usingHint(hint: string): SpecValidator
 }
